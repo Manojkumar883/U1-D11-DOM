8c84c0ef517ef22c5c741fb1082fa46933e59f92
Manojkumar883 <113252691+Manojkumar883@users.noreply.github.com>
2022-10-10 19:55:53 +0200

    html part

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+// function changeBackground(color) {
+//    document.body.style.background = color;
+// }
+
+// window.addEventListener("load",function() { changeBackground('#F1FAEE') });
+
+
+
+
+
+document.querySelector('html').style.backgroundColor = '#F1FAEE';
+
+// function changingHeader (){
+//     const headerNode = docments.querySelector('h1').innerText = "changed!"
+//     console.log(headerNode); 
+    
+// }
+document.getElementById("textChanger").onclick = function(){
+    document.getElementById("changingH1").innerText = "EURONICS PHONE SHOP";
+}
+
+
+
+
